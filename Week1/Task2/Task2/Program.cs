ff8a0f7a7691e5dbdf7a82e0ac8c761a8fcfbca3
Shalabayev <nurzhan99.ns@gmail.com>
2019-02-05 17:12:32 +0600

    check

---
+++ Week1/Task2/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student
+    {
+        public string name = "Alisher";
+        public string id = "17BD110639";
+        public int year = 2;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student first = new Student();
+            Console.WriteLine(first.name);
+            Console.WriteLine(first.id);
+            Console.WriteLine(first.year);
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Week1/Task3/Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static int[] MakeDoubleArray(int[] a)
+        {
+            int[] res = new int[a.Length * 2];
+            for(int i = 0; i < a.Length; ++i)
+            {
+                res[2 * i] = res[2 * i + 1] = a[i];
+            }
+            return res;
+        }
+
+        static int[] ReadData()
+        {
+            int x = int.Parse(Console.ReadLine());
+            string line = Console.ReadLine(); 
+            string[] parts = line.Split();
+            int[] t = new int[x];
+
+            for(int i = 0; i < x; i++)
+            {
+                t[i] = int.Parse(parts[i]);
+            }
+            return t;
+        } 
+
+        static void PrintAnswer(int[] ans)
+        {
+            foreach(var q in ans)
+            {
+                Console.Write(q + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] t = ReadData();
+            int[] ans = MakeDoubleArray(t);
+            PrintAnswer(ans);
+        }
+    }
+}
