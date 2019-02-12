294e8278fd91e7096d6ca852c0e24e482cc5f40e
Shalabayev <nurzhan99.ns@gmail.com>
2019-02-13 05:11:01 +0600

    Nurzhan

---
+++ Week2/Task4/Task4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathString = @"C:\Users\nurzh\Новая папка\path\Nurzhan.txt";
+            string fileName = "Nurzhan.txt";
+            if (!System.IO.File.Exists(pathString))
+            {
+                using (System.IO.FileStream fs = System.IO.File.Create(pathString))
+                {
+                    for (byte i = 0; i < 100; i++)
+                    {
+                        fs.WriteByte(i);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("File \"{0}\" already exists.", fileName);
+                return;
+            }
+            string destFile = @"C:\Users\nurzh\Новая папка\path1\Nurzhan.txt";
+            string sourceFile = @"C:\Users\nurzh\Новая папка\path\Nurzhan.txt";
+            System.IO.File.Copy(sourceFile, destFile, true);
+            if (System.IO.File.Exists(@"C:\Users\nurzh\Новая папка\path\Nurzhan.txt"))
+            {
+
+                try
+                {
+                    System.IO.File.Delete(@"C:\Users\nurzh\Новая папка\path\Nurzhan.txt");
+                }
+                catch (System.IO.IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
+
+        }
+    }
+}
